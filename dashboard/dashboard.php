e1975b65f545f846155e137c288aa6162aa098da
Ravikiran Gambhire Patil <37071665+RaviGambhire@users.noreply.github.com>
2018-08-22 11:11:20 +0530

    added dashboard

---
+++ dashboard/dashboard.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dashboard</title>
+	<meta charset='utf-8'>
+    <link rel="stylesheet" href="../CSS/bootstrap.min.css">
+</head>
+<body>
+<?php include('header.php') ?>
+<div class="container">
+	<div class="jumbotron">
+		<div class="row">
+			<div class="col-sm-8">
+			    <div class="row">
+			    	<div class="col-sm-6"><h3>Name : </h3></div>
+			    	<div class="col-sm-6"><h3>Anand</h3></div>
+			    </div>
+				<div class="row">
+			    	<div class="col-sm-6"><h3>Phone : </h3></div>
+			    	<div class="col-sm-6"><h3>9669696996</h3></div>
+			    </div>
+				<div class="row">
+			    	<div class="col-sm-6"><h3>Address : </h3></div>
+			    	<div class="col-sm-6"><h3>At Kumbhargaon Tq Biloli,Dist Nanded</h3></div>
+			    </div>	
+			</div>
+			<div class="col-sm-4">
+				<img src="../images/download.jpg" class="img-responsive img-circle" alt="Responsive image">
+			</div>
+		</div>
+		<div class="row">
+		     <h3>My Animals</h3>
+			<h3>To Buy</h3>
+			<div class="row">
+				<div class="col-sm-3"><a href="../sell/animalDetails.php"><img src="../images/download.jpg" class="img-responsive img-circle" alt="Responsive image"></a></div>
+				<div class="col-sm-3"><a href="../sell/animalDetails.php"><img src="../images/download.jpg" class="img-responsive img-circle" alt="Responsive image"></a></div>
+				<div class="col-sm-3"><a href="../sell/animalDetails.php"><img src="../images/download.jpg" class="img-responsive img-circle" alt="Responsive image"></a></div>
+				<div class="col-sm-3"><a href="../sell/animalDetails.php"><img src="../images/download.jpg" class="img-responsive img-circle" alt="Responsive image"></a></div>
+			</div>
+			
+		</div>
+		<div class="row">
+			<h3>To sell</h3>
+			<div class="row">
+				<div class="col-sm-3"><a href="../sell/animalDetails.php"><img src="../images/download.jpg" class="img-responsive img-circle" alt="Responsive image"></a></div>
+				<div class="col-sm-3"><a href="../sell/animalDetails.php"><img src="../images/download.jpg" class="img-responsive img-circle" alt="Responsive image"></a></div>
+				<div class="col-sm-3"><a href="../sell/animalDetails.php"><img src="../images/download.jpg" class="img-responsive img-circle" alt="Responsive image"></a></div>
+				<div class="col-sm-3"><a href="../sell/animalDetails.php"><img src="../images/download.jpg" class="img-responsive img-circle" alt="Responsive image"></a></div>
+			</div>
+		</div>
+	</div>	
+</div>
+</body>
+</html>
