90364a8831ef34f848b4cb947ff42c3156b752f6
anil <42891838+RachakondaAnil@users.noreply.github.com>
2018-09-02 17:34:32 +0530

    anil first commit

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="#">AnimalBazar.in</a>
+      <a class="navbar-brand" href="#">AnimalBazar</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
     <ul class="nav navbar-nav">
